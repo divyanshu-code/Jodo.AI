69814d194560673df97aede75010e5b8e1d3c451
Divyanshu Bisht <divyanshubisht5734@gmail.com>
2026-05-25 00:41:19 +0530

    refactor: update server listen method to specify host

--- toolcalling/server.js
+++ toolcalling/server.js
@@ -55,7 +55,6 @@ app.post('/chat', async (req, res) => {
 
 })
 
-app.listen(port, () => {
-    console.log(`Server running on the port ${port}`);
-
+app.listen(port, '0.0.0.0', () => {      
+    console.log(`Server running on port ${port}`);
 })
